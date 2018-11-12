728ef0e062dfb15569dcb29e6748ad34f23f2526
NanaOtsuka <ab15023@shibaura-it.ac.jp>
2018-11-12 14:55:25 +0900

    rewrite

--- Threshold_measurement/src/Threshold_measurement.cpp
+++ Threshold_measurement/src/Threshold_measurement.cpp
@@ -14,26 +14,7 @@
 #include<fstream>
 #include<string>
 
-float data3[9999] = {0.0};
-float data4[9999] = { 0.0 };
-float data5[9999] = { 0.0 };
 float gai[8] = { 0.0 };
-float gai3 = 0;
-float gai4 = 0;
-float gai5 = 0;
-float threshold = 0;
-int th_i =1;
-int a;
-int x = 0;
-float data_max = 0;
-
-float max = 0;
-float max3 = 0;
-float max4 = 0;
-float max5 = 0;
-int sign;
-
-bool thresholdflag =1;
 
 // Module specification
 // <rtc-template block="module_spec">
@@ -129,15 +110,12 @@ RTC::ReturnCode_t Threshold_measurement::onActivated(RTC::UniqueId ec_id)
 {
 	std::cout << "------Activate------" << std::endl;
 
-	a=0;
-
 	FILE *fp1;
 	fp1 = fopen("Threshold measurement.csv", "w");
 
 	fprintf(fp1, "ain3[N],ain4[N],ain5[N]\n", gai[3], gai[4], gai[5]);
 	fclose(fp1);
 
-	 data4[9999] = { 0.0 };
   return RTC::RTC_OK;
 }
 
@@ -150,64 +128,49 @@ RTC::ReturnCode_t Threshold_measurement::onDeactivated(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t Threshold_measurement::onExecute(RTC::UniqueId ec_id)
 {
+	float data3[9999] = { 0.0 };
+	float data4[9999] = { 0.0 };
+	float data5[9999] = { 0.0 };
+	float threshold = 0;
+	int th_i = 1;
+	float max = 0;
+	float max3 = 0;
+	float max4 = 0;
+	float max5 = 0;
+	int sign;
 
-
+	//測定開始・終了タイミングの決定
 	    m_signIn.isNew();
 		m_signIn.read();
 		sign = m_sign.data;
-		std::cout << sign << std::endl;
 
+		//閾値計算用データ測定
 		if (sign == 1&&threshold==0){
-			    
-
+			
 				std::cout << sign << std::endl;
 
 						m_gainer2In.isNew();
 						m_gainer2In.read();
+						//データ保存用
 						gai[3] = m_gainer2.data[3];
 						gai[4] = m_gainer2.data[4];
 						gai[5] = m_gainer2.data[5];
-
-						gai3 = m_gainer2.data[3];
-						gai4 = m_gainer2.data[4];
-						gai5 = m_gainer2.data[5];
-						
-						/*printf("\rgai[4] = %3.3f\n", gai[4]);*/
-
-						data3[th_i] = gai3;
-						data4[th_i] = gai4;
-						data5[th_i] = gai5;
-
-						/*printf("\rdata3[10] = %f\n", data3[10]);
-						printf("\rdata4[10] = %f\n", data4[10]);
-						printf("\rdata5[10] = %f\n", data5[10]);*/
-
+						//閾値計算用
+						data3[th_i] = m_gainer2.data[3];
+						data4[th_i] = m_gainer2.data[4];
+						data5[th_i] = m_gainer2.data[5];
+						//配列用		
 						++th_i;
-
-						
-						
-						
-
 
 						//記録用ファイル生成
 						FILE *fp1;
-
 						fp1 = fopen("Threshold measurement.csv", "a");
-
 						fprintf(fp1, "%3.3f,%3.3f,%3.3f\n",  gai[3], gai[4], gai[5]);
 						fclose(fp1);
 
-						m_signIn.isNew();
-						m_signIn.read();
-						sign = m_sign.data;
-						/*printf("\rsign = %d\n", sign);*/
-
-						
-
-			
 
 		};
-
+		//閾値計算
 	  if (sign == 2){
 
 		  for (int i = 0; i <= th_i; i++){
@@ -231,27 +194,18 @@ RTC::ReturnCode_t Threshold_measurement::onExecute(RTC::UniqueId ec_id)
 
 		  }
 
-		/*  printf("\rmax3 = %3.3f\n", max3);
-		  printf("\rmax4 = %3.3f\n", max4);
-		  printf("\rmax5 = %3.3f\n", max5);*/
-
-
 			max = max3;
 			if (max4 > max)max = max4;
 			if (max5 > max)max = max5;
-
-		  	//fclose(fp1);
 
 		  	//閾値計算
 		  	threshold = max*0.70;
 		  	printf("\rthreshold = %3.3f\n", threshold);
 		  	m_threshold.data = threshold;
 		  	m_thresholdOut.write();
-
-			
-
 	  };
 
+	  //リセット
 	  if (sign == 3){
 		    th_i = 1;
 		    data3[9999] = { 0.0 };
@@ -261,20 +215,12 @@ RTC::ReturnCode_t Threshold_measurement::onExecute(RTC::UniqueId ec_id)
 			max3 = 0;
 			max4 = 0;
 			max5 = 0;
-			thresholdflag = 0;
 		    threshold = 0;
 
 		  	m_threshold.data = threshold;
 		  	m_thresholdOut.write();
 	  };
 	  
-	
-	  printf("\rthreshold = %3.3f\n", threshold);
-	  printf("\rsign = %d\n", sign);
-
-	 
-	
-	
   return RTC::RTC_OK;
 }
 
